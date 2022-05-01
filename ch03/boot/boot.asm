6dbc7d81509d56e68f9b3d0cecf9712aa45c49d5
sosoariar <sosoariar@163.com>
2022-05-01 14:14:42 +0800

    更新注释

--- ch03/boot/boot.asm
+++ ch03/boot/boot.asm
@@ -60,31 +60,31 @@ rd_disk_m_16:
     mov al,cl
     out dx,al            ;读取的扇区数
     mov eax,esi	   ;恢复ax
-; 往该通道上的三个LBA寄存器写入扇区起始地址的低24位
-    ;LBA地址7~0位写入端口0x173
-    mov dx,0x173
-    out dx,al
 
+; 往下列地址写入数据,可以初始化硬盘的读数据状态
+    ;LBA地址7~0位写入端口0x1f3
+    mov dx,0x1f3
+    out dx,al
     ;LBA地址15~8位写入端口0x1f4
     mov cl,8
     shr eax,cl
-    mov dx,0x174
+    mov dx,0x1f4
     out dx,al
-
     ;LBA地址23~16位写入端口0x1f5
     shr eax,cl
-    mov dx,0x175
+    mov dx,0x1f5
     out dx,al
+
     ; 往device 寄存器中写入LBA 地址的24～27 位，并置第6 位为l ，使其为LBA 模式，设置第4
     ;位，选择操作的硬盘（ master 硬盘或slave 硬盘）。
     shr eax,cl
     and al,0x07	   ;lba第24~27位
     or al,0xe0	   ; 设置7～4位为1110,表示lba模式
-    mov dx,0x176
+    mov dx,0x1f6
     out dx,al
 
 ;第3步：向硬盘发出读命令，0x20
-    mov dx,0x177
+    mov dx,0x1f7
     mov al,0x20
     out dx,al
 
@@ -98,13 +98,22 @@ rd_disk_m_16:
       jnz .not_ready	   ;若未准备好，继续等。
 
 ;第5步：硬盘状态准备好后,开始从指定端口读取数据
+;0x1f0~0x1f2 只有16位,即2字节的data缓冲,即每次in操作只读入2字节
+;di为要读取的扇区数，一个扇区有512字节，每次读入2字节
+; 共需di*512/2次，所以di*256
       mov ax, di
       mov dx, 256
       mul dx
-      mov cx, ax	   ; di为要读取的扇区数，一个扇区有512字节，每次读入一个字，
-			   ; 共需di*512/2次，所以di*256
-      mov dx, 0x170
-
+      mov cx, ax
+;初始化数据读回地址
+      mov dx, 0x1f0
+;通过ox1f0端口读的数据顺序写入到指定地址0x900为起始地址的内存
+  .go_on_read:
+      in ax,dx
+      mov [bx],ax
+      add bx,2
+      loop .go_on_read
+      ret
    times 510-($-$$) db 0
    db 0x55,0xaa
 
