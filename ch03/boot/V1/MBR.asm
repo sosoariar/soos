81c2fd51dad4036a50e6a0e6cede9ff3045247fd
sosoariar <sosoariar@163.com>
2022-06-19 11:26:29 +0800

    MBR 主引导记录-- 使用硬盘

--- ch03/boot/V1/MBR.asm
+++ ch03/boot/V1/MBR.asm
@@ -1,50 +1,47 @@
-; 功能: 在屏幕上显示字符
-SECTION MBR vstart=0x7c00 ; 源代码编译后在内存中的起始地址
-   mov ax,cs              ; CS:IP 默认是顺序执行的,同样这条指令也会顺序存储在0x7c00向上扩展的地址中
+;;;; 在屏幕上显示字符 ;;;;
+; 0x7c00 是一种规范,BIOS功能包含了加载MBR中代码到ox7c00, CS:IP 会被BIOS设置为0X7C00
+; MBR 的可执行代码逻辑在内存地址使用上需要以 0X7C00 为基础
+SECTION MBR vstart=0x7c00
+   mov ax,cs                            ; CS被BIOS设置为0, 下列的代码是初始化为0的操作
    mov ds,ax
    mov es,ax
    mov ss,ax
    mov fs,ax
    mov sp,0x7c00
-   mov ax,0xb800
+   mov ax,0xb800                        ; 与VGA显示有关
    mov gs,ax
 
+;;;; BIOS中断处理程序,与显示字符有关 ;;;;
 ; BIOS 0x10 中断
-; 从寄存器AX,BX,CX,DX中得到需要执行什么功能
-
+; 中断处理程序功能的执行需要参数,而参数约定从寄存器AX,BX,CX,DX中得到
 ; 功能号0x60 清屏+初始化屏幕大小,具体数据代表的内容可以help
    mov ax, 0x0600
    mov bx, 0x0700
-   mov cx, 0           ;
-   mov dx, 0x184f	     ;
+   mov cx, 0
+   mov dx, 0x184f
    int 0x10            ; int 0x10
 
-; 直接操作显存地址
-    mov byte [gs:0x00],'H'
-    mov byte [gs:0x01],0X07
+; 功能号: 0x03 光标相关
+   mov ah, 3
+   mov bh, 0
+   int 0x10
 
-    mov byte [gs:0x02],'E'
-    mov byte [gs:0x03],0X07
+; 功能号: 0x13 打印字符串
+   mov ax, message
+   mov bp, ax		; 对应功能号中断触发时,显示的数据从 ES:BP获得
 
-    mov byte [gs:0x04],'L'
-    mov byte [gs:0x05],0X07
+   mov cx, 10
+   mov ax, 0x1301
+   mov bx, 0x2
+   int 0x10
 
-    mov byte [gs:0x06],'L'
-    mov byte [gs:0x07],0X07
+;;;; 程序悬停 ;;;;
+; CS,IP重复指向该地址,无法往下执行,
+; 即在寄存器和内存数据保持不变时
+   jmp $
 
-    mov byte [gs:0x08],'O'
-    mov byte [gs:0x09],0X07
+   message db "Hello MBR!"
 
-    mov byte [gs:0x0A],'M'
-    mov byte [gs:0x0B],0X07
-
-    mov byte [gs:0x0C],'B'
-    mov byte [gs:0x0D],0X07
-
-    mov byte [gs:0x0E],'R'
-    mov byte [gs:0x0F],0X07
-
-   jmp $	;程序悬停
-
+; MBR 约定,必须写满 512Byte 且结尾是 0101 1010
    times 510-($-$$) db 0
    db 0x55,0xaa
--- ch03/boot/V1/run.sh
+++ ch03/boot/V1/run.sh
@@ -1,1 +1,4 @@
-bochs -f ./bochsrc.disk
+#!/bin/bash
+# bochs 需要添加在系统 path中
+
+bochs -f bochsrc.disk
