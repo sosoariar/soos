f913be34b34e7fc1721146c2f26d37284789e0c6
sosoariar <sosoariar@163.com>
2022-06-19 19:49:09 +0800

    ch05 内核 开启分页

--- ch05/V2/boot/loader.asm
+++ ch05/V2/boot/loader.asm
@@ -14,8 +14,8 @@ GDT_BASE:
 
 ; 代码段描述符
 CODE_DESC:
-    dd    0x0000FFFF        ;低4字节
-    dd    DESC_CODE_HIGH4   ;高4字节
+    dd    0x0000FFFF
+    dd    DESC_CODE_HIGH4
 
 ; 数据段和栈段描述符
 ; 数据段和栈段共同使用一个段描述符,栈是向下扩展,数据段是向上扩展
@@ -38,9 +38,7 @@ SELECTOR_CODE   equ     (0x0001<<3) + TI_GDT + RPL0     ; (CODE_DESC - GDT_BASE)
 SELECTOR_DATA   equ     (0x0002<<3) + TI_GDT + RPL0	    ; 同上
 SELECTOR_VIDEO  equ     (0x0003<<3) + TI_GDT + RPL0	    ; 同上
 
-; total_mem_bytes用于保存内存容量,以字节为单位,此位置比较好记。
-; 当前偏移loader.bin文件头0x200字节,loader.bin的加载地址是0x900,
-; 故total_mem_bytes内存中的地址是0xb00.将来在内核中咱们会引用此地址
+; total_mem_bytes用于保存内存容量
 total_mem_bytes dd 0
 
 ; 定义全局描述符表 GDT 的指针,此指针是 lgdt 加载 GDT 到 gdtr 寄存器时用的
@@ -50,69 +48,60 @@ gdt_ptr
     dd  GDT_BASE    ; GDT 的32位起始地址
 
 ;定义一个缓冲区
-;人工对齐:total_mem_bytes4字节+gdt_ptr6字节+ards_buf244字节+ards_nr2,共256字节
 ards_buf times 244 db 0
 ards_nr dw 0		      ;用于记录ards结构体数量
 
-loadermsg db '2 loader in real.'
-
 loader_start:
 
-;-------  int 15h eax = 0000E820h ,edx = 534D4150h ('SMAP') 获取内存布局  -------
+;;;; 内存获取并缓存到 total_mem_bytes ;;;;
 ;检测功能的强弱 0xe820 0xe801 0x88
-   xor ebx, ebx		      ;第一次调用时，ebx值要为0
-   mov edx, 0x534d4150	  ;edx只赋值一次，循环体中不会改变
-   mov di, ards_buf	      ;ards结构缓冲区
-
-.e820_mem_get_loop:	      ;循环获取每个ARDS内存范围描述结构
-   mov eax, 0x0000e820	  ;执行int 0x15后,eax值变为0x534d4150,所以每次执行int前都要更新为子功能号。
-   mov ecx, 20		      ;ARDS地址范围描述符结构大小是20字节
+   xor ebx, ebx
+   mov edx, 0x534d4150
+   mov di, ards_buf
+.e820_mem_get_loop:
+   mov eax, 0x0000e820
+   mov ecx, 20
    int 0x15
-   jc .e820_failed_so_try_e801   ;若cf位为1则有错误发生，尝试0xe801子功能
-   add di, cx		      ;使di增加20字节指向缓冲区中新的ARDS结构位置
-   inc word [ards_nr]	  ;记录ARDS数量
-   cmp ebx, 0		      ;若ebx为0且cf不为1,这说明ards全部返回，当前已是最后一个
+   jc .e820_failed_so_try_e801
+   add di, cx
+   inc word [ards_nr]
+   cmp ebx, 0
    jnz .e820_mem_get_loop
 
-;在所有ards结构中，找出(base_add_low + length_low)的最大值，即内存的容量。
-   mov cx, [ards_nr]	  ;遍历每一个ARDS结构体,循环次数是ARDS的数量
+   mov cx, [ards_nr]
    mov ebx, ards_buf
-   xor edx, edx		      ;edx为最大的内存容量,在此先清0
-.find_max_mem_area:	      ;无须判断type是否为1,最大的内存块一定是可被使用
-   mov eax, [ebx]	      ;base_add_low
-   add eax, [ebx+8]	      ;length_low
-   add ebx, 20		      ;指向缓冲区中下一个ARDS结构
-   cmp edx, eax		      ;冒泡排序,找出最大,edx寄存器始终是最大的内存容量
+   xor edx, edx
+.find_max_mem_area:
+   mov eax, [ebx]
+   add eax, [ebx+8]
+   add ebx, 20
+   cmp edx, eax
    jge .next_ards
-   mov edx, eax		      ;edx为总内存大小
+   mov edx, eax
 .next_ards:
    loop .find_max_mem_area
    jmp .mem_get_ok
 
 ; ------  int 15h ax = E801h 获取内存大小,最大支持4G  ------
-; 返回后, ax cx 值一样,以KB为单位,bx dx值一样,以64KB为单位
-; 在ax和cx寄存器中为低16M,在bx和dx寄存器中为16MB到4G。
 .e820_failed_so_try_e801:
    mov ax,0xe801
    int 0x15
-   jc .e801_failed_so_try88   ;若当前e801方法失败,就尝试0x88方法
+   jc .e801_failed_so_try88
 
-;1 先算出低15M的内存,ax和cx中是以KB为单位的内存数量,将其转换为以byte为单位
-   mov cx,0x400	     ;cx和ax值一样,cx用做乘数
+   mov cx,0x400
    mul cx
    shl edx,16
    and eax,0x0000FFFF
    or edx,eax
-   add edx, 0x100000 ;ax只是15MB,故要加1MB
-   mov esi,edx	     ;先把低15MB的内存容量存入esi寄存器备份
+   add edx, 0x100000
+   mov esi,edx
 
-;2 再将16MB以上的内存转换为byte为单位,寄存器bx和dx中是以64KB为单位的内存数量
    xor eax,eax
    mov ax,bx
-   mov ecx, 0x10000	;0x10000十进制为64KB
-   mul ecx		;32位乘法,默认的被乘数是eax,积为64位,高32位存入edx,低32位存入eax.
-   add esi,eax		;由于此方法只能测出4G以内的内存,故32位eax足够了,edx肯定为0,只加eax便可
-   mov edx,esi		;edx为总内存大小
+   mov ecx, 0x10000
+   mul ecx
+   add esi,eax
+   mov edx,esi
    jmp .mem_get_ok
 
 ;-----------------  int 15h ah = 0x88 获取内存大小,只能获取64M之内  ----------
@@ -167,19 +156,88 @@ loader_print_32:
     mov ax, SELECTOR_VIDEO
     mov gs, ax
 
-    mov byte [gs:0x0C],' '
-    mov byte [gs:0x0D],0X07
+; 创建页目录及页表并初始化页内存位图
+   call setup_page
 
-    mov byte [gs:0x0E],' '
-    mov byte [gs:0x0F],0X07
+; 要将描述符表地址及偏移量写入内存gdt_ptr,一会用新地址重新加载
+   sgdt [gdt_ptr]
 
-    mov byte [gs:0x10],'P'
-    mov byte [gs:0x11],0X07
+; 将gdt描述符中视频段描述符中的段基址+0xc0000000
+   mov ebx, [gdt_ptr + 2]
+   or dword [ebx + 0x18 + 4], 0xc0000000
 
-    mov byte [gs:0x12],'R'
-    mov byte [gs:0X13],0X07
+;将gdt的基址加上0xc0000000使其成为内核所在的高地址
+   add dword [gdt_ptr + 2], 0xc0000000
 
-    mov byte [gs:0x14],'O'
-    mov byte [gs:0x15],0X07
+   add esp, 0xc0000000
+   mov eax, PAGE_DIR_TABLE_POS
+   mov cr3, eax
 
-    jmp $
+; 打开cr0的pg位(第31位)
+   mov eax, cr0
+   or eax, 0x80000000
+   mov cr0, eax
+
+;在开启分页后,用gdt新的地址重新加载
+   lgdt [gdt_ptr]             ; 重新加载
+
+   mov byte [gs:160], 'V'
+   mov byte [gs:162], 'i'
+   mov byte [gs:164], 'r'
+   mov byte [gs:166], 't'
+   mov byte [gs:168], 'u'
+   mov byte [gs:170], 'a'
+   mov byte [gs:172], 'l'
+
+   jmp $
+
+;-------------   创建页目录及页表   ---------------
+setup_page:
+    ;先把页目录占用的空间逐字节清0
+    mov ecx, 4096
+    mov esi, 0
+.clear_page_dir:
+   mov byte [PAGE_DIR_TABLE_POS + esi], 0
+   inc esi
+   loop .clear_page_dir
+
+;开始创建页目录项(PDE)
+.create_pde:
+   mov eax, PAGE_DIR_TABLE_POS
+   add eax, 0x1000 			     ; 此时eax为第一个页表的位置及属性
+   mov ebx, eax
+
+;   下面将页目录项0和0xc00都存为第一个页表的地址，
+;   一个页表可表示4MB内存,这样0xc03fffff以下的地址和0x003fffff以下的地址都指向相同的页表，
+;   这是为将地址映射为内核地址做准备
+   or eax, PG_US_U | PG_RW_W | PG_P	            ; 页目录项的属性RW和P位为1,US为1,表示用户属性,所有特权级别都可以访问.
+   mov [PAGE_DIR_TABLE_POS + 0x0], eax          ; 第1个目录项,在页目录表中的第1个目录项写入第一个页表的位置(0x101000)及属性(7)
+   mov [PAGE_DIR_TABLE_POS + 0xc00], eax        ; 一个页表项占用4字节,0xc00表示第768个页表占用的目录项,0xc00以上的目录项用于内核空间,
+                                                ; 也就是页表的0xc0000000~0xffffffff共计1G属于内核,0x0~0xbfffffff共计3G属于用户进程.
+   sub eax, 0x1000
+   mov [PAGE_DIR_TABLE_POS + 4092], eax	     ; 使最后一个目录项指向页目录表自己的地址
+
+;下面创建页表项(PTE)
+   mov ecx, 256				     ; 1M低端内存 / 每页大小4k = 256
+   mov esi, 0
+   mov edx, PG_US_U | PG_RW_W | PG_P	     ; 属性为7,US=1,RW=1,P=1
+.create_pte:				     ; 创建Page Table Entry
+   mov [ebx+esi*4],edx			     ; 此时的ebx已经在上面通过eax赋值为0x101000,也就是第一个页表的地址
+   add edx,4096      ; edx
+   inc esi
+   loop .create_pte
+
+;创建内核其它页表的PDE
+   mov eax, PAGE_DIR_TABLE_POS
+   add eax, 0x2000
+   or  eax, PG_US_U | PG_RW_W | PG_P
+   mov ebx, PAGE_DIR_TABLE_POS
+   mov ecx, 254			     ; 范围为第769~1022的所有目录项数量
+   mov esi, 769
+.create_kernel_pde:
+   mov [ebx+esi*4], eax
+   inc esi
+   add eax, 0x1000
+   loop .create_kernel_pde
+   ret
+
