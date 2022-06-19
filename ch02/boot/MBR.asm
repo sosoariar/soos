a195b2e80a8471425ba629f7b49c32c98023d6a4
sosoariar <sosoariar@163.com>
2022-06-19 11:12:00 +0800

    MBR 主引导记录-- 利用BIOS中断显示字符

--- ch02/boot/MBR.asm
+++ ch02/boot/MBR.asm
@@ -1,5 +1,5 @@
 ;;;; 在屏幕上显示字符 ;;;;
-; 0x7c00 是一种规范,BIOS功能包含了加载MBR中代码代码到ox7c00, CS:IP 会被BIOS设置为0X7C00
+; 0x7c00 是一种规范,BIOS功能包含了加载MBR中代码到ox7c00, CS:IP 会被BIOS设置为0X7C00
 ; MBR 的可执行代码逻辑在内存地址使用上需要以 0X7C00 为基础
 SECTION MBR vstart=0x7c00
    mov ax,cs                            ; CS被BIOS设置为0, 下列的代码是初始化为0的操作
@@ -11,14 +11,14 @@ SECTION MBR vstart=0x7c00
    mov ax,0xb800                        ; 与VGA显示有关
    mov gs,ax
 
+;;;; BIOS中断处理程序,与显示字符有关 ;;;;
 ; BIOS 0x10 中断
-; 从寄存器AX,BX,CX,DX中得到需要执行什么功能
-
+; 中断处理程序功能的执行需要参数,而参数约定从寄存器AX,BX,CX,DX中得到
 ; 功能号0x60 清屏+初始化屏幕大小,具体数据代表的内容可以help
    mov ax, 0x0600
    mov bx, 0x0700
-   mov cx, 0           ;
-   mov dx, 0x184f	     ;
+   mov cx, 0
+   mov dx, 0x184f
    int 0x10            ; int 0x10
 
 ; 功能号: 0x03 光标相关
@@ -30,13 +30,18 @@ SECTION MBR vstart=0x7c00
    mov ax, message
    mov bp, ax		; 对应功能号中断触发时,显示的数据从 ES:BP获得
 
-   mov cx, 10		; 显示的字符数
+   mov cx, 10
    mov ax, 0x1301
    mov bx, 0x2
    int 0x10
 
-   jmp $	;程序悬停
+;;;; 程序悬停 ;;;;
+; CS,IP重复指向该地址,无法往下执行,
+; 即在寄存器和内存数据保持不变时
+   jmp $
 
    message db "Hello MBR!"
+
+
    times 510-($-$$) db 0
    db 0x55,0xaa
